8caa98a60d88dd0d82cb537856ccba9b31decb3b
GiorgiQenqadze <giorgagiorgi133@gmail.com>
2025-02-04 19:31:59 +0400

    first commit

---
+++ day 1/lessen1.py
@@ -0,0 +1,19 @@
+name = "giorgi"
+#name არის ცვლადი
+# = არის ცვლადისთვის მნიშვნელობის მიმნიჭებელი სიმბოლო
+# "giorgi" არის ცვლადის მნიშვნელობა
+
+surname = "qenqadze"
+
+#print(name)
+#პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+name = "giorgi" 
+age = 13
+haight = 166.5
+knows_programming = True
+is_Ugly = False
+isUgly = False # ჯავასკრიფტული cameLcase
+print(name + " " + surname)
+# სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოები
+print(name + age)
+print(name + " " + str(age))
