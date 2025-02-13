6e5e627e7a955d2cd2e60f065b7804999882733e
GiorgiQenqadze <giorgagiorgi133@gmail.com>
2025-02-13 19:02:52 +0400

    second commit

--- day 1/homework/hw.py
+++ day 1/homework/hw.py
@@ -1,4 +1,4 @@
-name = "giorgi"
+
 #name არის ცვლადი
 # = არის ცვლადისთვის მნიშვნელობის მიმნიჭებელი სიმბოლო
 # "giorgi" არის ცვლადის მნიშვნელობა
@@ -9,11 +9,13 @@ surname = "qenqadze"
 #პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
 name = "giorgi" 
 age = 13
-haight = 166.5
+haight = "166.5"
 knows_programming = True
+live_in="didi_digomi"
 is_Ugly = False
 isUgly = False # ჯავასკრიფტული cameLcase
 print(name + " " + surname)
 # სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოები
-print(name + age)
+
 print(name + " " + str(age))
+print("me var" + " " + name + surname + "var "+str(age)+"wlis"+"simagleshi var "+haight+" vcxovrob"+live_in)
