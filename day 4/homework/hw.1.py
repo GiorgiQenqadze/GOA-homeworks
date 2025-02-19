10b9048ce7864788c0610030b967f198cee3079f
GiorgiQenqadze <giorgagiorgi133@gmail.com>
2025-02-19 17:03:14 +0400

    second commit

---
+++ day 4/homework/hw.1.py
@@ -0,0 +1,9 @@
+ #შექმენით 3 ცვლადი სადაც შეინახავთ თქვენთვის სასურველ მონაცემს. type ფუნქციის გამოყენებით დაბეჭდეთ მათი ტიპი
+
+fullname ="giorgi qenqadze"
+hight =1.66
+age =13
+
+print(type(fullname))
+print(type(hight))
+print(type(age))
---
+++ day 4/homework/hw3.py
@@ -0,0 +1,7 @@
+# რომელია სწორი და რატომ:
+# print("Hello World!')
+# print('Hello' World!')
+# print("Hello World")
+
+print("Hello World")
+#რადგან hello world დაწერილია ორმაგ ბრჭყალებში
