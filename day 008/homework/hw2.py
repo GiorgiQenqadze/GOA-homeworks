a03e043399a4393f14e133f4dd7e16f87cb47ee2
GiorgiQenqadze <giorgioo70@gmail.com>
2025-03-05 21:30:26 +0400

    second commit

--- day 008/homework/hw2.py
+++
@@ -1,7 +0,0 @@
-#4) რას გამოიტანს შემდეგი კოდი და რატომ:
-number1 = 10
-number2 = 20
-sum_of_numbers = number1 + number2
-print("sum_Of_numbers")
-
-#აქ გამოიტანს 30 იმიტომ რომ number1 და number2 შეიკრიბება
