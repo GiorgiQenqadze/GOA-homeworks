9adccc7d6cc8c69624ec1a387d27c86fb47aa6c1
GiorgiQenqadze <giorgioo70@gmail.com>
2025-03-05 19:38:17 +0400

    second commit

---
+++ day 008/classwork/cw.py
@@ -0,0 +1,23 @@
+#1)რამდენი ცვლადი შეიქმნება ამ კოდის მიხედვით?(აღწერეთ რატომ)
+naMe = "Luka"
+NAME = "Data"
+naME = "Vano"
+name = "Demetre"
+NaMe = "Jan"
+name = "Giorgi"
+
+
+name = "demetre"
+name = "giorgi"
+#2) შექმენით 5 ცვლადი, snake_case-ის გამოყენებით და დაბეჭდეთ
+
+my_name="giorgi"
+my_age=13
+my_favorit_color="black"
+my_favorit_food="burger"
+my_favorit_car="bmw"
+print(my_name)
+print(my_age)
+print(my_favorit_color)
+print(my_favorit_food)
+print(my_favorit_car)
---
+++ day 008/homework/hw2.py
@@ -0,0 +1,7 @@
+#4) რას გამოიტანს შემდეგი კოდი და რატომ:
+number1 = 10
+number2 = 20
+sum_of_numbers = number1 + number2
+print("sum_Of_numbers")
+
+#აქ გამოიტანს 30 იმიტომ რომ number1 და number2 შეიკრიბება
