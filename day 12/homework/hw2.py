360369db883a8614598562e668051010928b1501
GiorgiQenqadze <giorgioo70@gmail.com>
2025-03-19 23:18:46 +0400

    second commit

---
+++ day 12/homework/hw2.py
@@ -0,0 +1,9 @@
+#შექმენით 2 ცვლადი, ერთში შეინახეთ სთრინგ რიცხვი, მეორეში integer-ი .
+#  თქვენი დავალებაა მათ შორის მოახდინოთ ყველა მათემატიკური ოპერაცია(+, -, *, /)
+
+str_num = "10"
+int_num = 5
+addition = str_num + int_num
+subtraction = str_num - int_num
+multiplication = str_num * int_num
+division = str_num / int_num
---
+++ day 12/homework/hw3.py
@@ -0,0 +1,4 @@
+#მომხმარებელს შემოატანინეთ სიმაღლე, გადააქციეთ float-ად და დაბეჭდეთ მისი მონაცემთა ტიპი
+
+hight = float(input("enter your hight: "))
+print(type(hight))
