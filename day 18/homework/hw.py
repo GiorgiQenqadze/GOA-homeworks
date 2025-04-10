04637cbcfdddcbbb2dc8ced3d0fc1d1956efe890
GiorgiQenqadze <giorgioo70@gmail.com>
2025-04-10 16:19:05 +0400

    second commit

---
+++ day 18/homework/hw.py
@@ -0,0 +1,19 @@
+#მომხმარებელს შემოატანინეთ მისი ასაკი და სახელი, შემდეგ if/else-ის გამოყენებით შეამოწმეთ არის თუ არა ის სრულწლოვანი და უდრის თუ არა მისი სახელი თქვენს სახელს.(შემოწმების შედეგიდან გამომდინარე დაბეჭდეთ True/False)
+
+
+age=int(input("how old are you?: "))
+name=input("enter your name: ")
+
+name1="giorgi"
+
+if age >= 18:
+    print(True)
+else: 
+    print(False)
+
+
+
+if name == name1:
+    print(True)
+else:
+    print(False)
---
+++ day 18/homework/hw1.py
@@ -0,0 +1,8 @@
+#მომხმარებელს შემოატანინეთ სახელი და თუ ის თქვენს სახელს ემთხვევა დაბეჭდეთ "coincidence"
+
+name=input("what is your name: ")
+name1="giorgi"
+
+
+if name == name1:
+    print("coincidence")
