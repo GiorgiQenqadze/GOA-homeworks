bf4e1c853fde450966ba6fc94c072eb91722125f
GiorgiQenqadze <giorgioo70@gmail.com>
2025-05-24 13:41:58 +0400

    second commit

---
+++ day 30/classwork/cw.py
@@ -0,0 +1,7 @@
+#შექმენით 5 ელემენტიანი სია, სადაც ჩაწერთ თქვენს საყვარელი მანქანების სახელს
+
+
+
+car_brends=["BMW","Mersedes","Porche","Ferari","Lamborgini"]
+
+print(car_brends)
---
+++ day 30/classwork/cw2.py
@@ -0,0 +1,8 @@
+#შექმენით 5 ელემენტიანი სია და დაბეჭდეთ მისი მე-4 ელემენტი
+
+
+
+my_hobys=["Games","MMA","Programing","cars","Motorcycle"]
+
+
+print(my_hobys[3])
